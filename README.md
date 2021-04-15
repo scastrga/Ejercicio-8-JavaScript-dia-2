4ec7ef56b4f93986e67fa705f1ced05effc4a2f1
scastrga <scastrga@everis.com>
2021-04-15 20:44:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vgzrrlejercicio8-edv1vb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vgzrrlejercicio8-edv1vb)
---
+++ index.js
@@ -0,0 +1,8 @@
+var parrafos = document.getElementsByTagName("p");
+
+var primerParr = parrafos[0];
+var ultimoParr = parrafos[parrafos.length - 1];
+
+console.log(parrafos);
+console.log(primerParr);
+console.log(ultimoParr);
